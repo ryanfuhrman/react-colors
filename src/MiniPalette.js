40edcdc8f79729ff9803dc9e22cee53b2acc7afe
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-10-01 10:45:20 -0400

    Start styling PaletteList withStyles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/styles';
 
 const styles = {
   root: {
-    borderColor: "white",
+    backgroundColor: "white",
     border: "1px solid black",
     borderRadius: "5px",
     padding: "0.5rem",
