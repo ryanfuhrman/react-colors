8cb48ea43f7f562ddde8ade844e7c3d8bc8b5bed
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-10-21 15:50:10 -0400

    Move styles out and add constants file

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,25 +1,9 @@
 import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
-
-const styles = {
-  picker: {
-    width: "100% !important",
-    marginTop: "2rem",
-  },
-  addColor: {
-    width: "100%",
-    padding: "1rem",
-    marginTop: "1rem",
-    fontSize: "2rem",
-  },
-  colorNameInput: {
-    width: "100%",
-    height: "70px",
-  },
-};
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles/ColorPickerFormStyles";
 
 class ColorPickerForm extends Component {
   constructor(props) {
