60420899c7ef86dfbe202f3c6f2fe731a46ce8c9
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-11-03 16:25:42 -0500

    Prevent duplicate random colors

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -58,8 +58,16 @@ class NewPaletteForm extends Component {
   }
   addRandomColor() {
     const allColors = this.props.palettes.map(p => p.colors).flat();
-    const rand = Math.floor(Math.random() * allColors.length);
-    const randomColor = allColors[rand];
+    let rand;
+    let randomColor = allColors[rand];
+    let isDuplicateColor = true;
+    while (isDuplicateColor) {
+      rand = Math.floor(Math.random() * allColors.length);
+      randomColor = allColors[rand];
+      isDuplicateColor = this.state.colors.some(
+        color => color.name === randomColor.name
+      );
+    }
     this.setState({ colors: [...this.state.colors, randomColor] });
   }
   handleSubmit(newPalette) {
